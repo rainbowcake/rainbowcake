e2b8d7c84468694357a2845e8c3969b0c434646a
Braun Márton Szabolcs <braun.marton@autsoft.hu>
2019-01-14 13:39:18 +0100

    Fix dummy NoAnimation visibility

--- rainbow-cake/src/main/java/hu/autsoft/rainbowcake/navigation/NoAnimation.kt
+++ rainbow-cake/src/main/java/hu/autsoft/rainbowcake/navigation/NoAnimation.kt
@@ -2,7 +2,7 @@ package hu.autsoft.rainbowcake.navigation
 
 import android.view.animation.Animation
 
-object NoAnimation : Animation() {
+internal object NoAnimation : Animation() {
 
     init {
         duration = 0
