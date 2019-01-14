f3d4020ff74b89b25d1e2bb6077fd4ee52234e71
Braun Márton Szabolcs <braun.marton@autsoft.hu>
2019-01-14 13:30:03 +0100

    Add more example Fragments and example animations to demo app

--- demo/src/main/java/hu/autsoft/rainbowcake/demo/di/ViewModelModule.kt
+++ demo/src/main/java/hu/autsoft/rainbowcake/demo/di/ViewModelModule.kt
@@ -4,7 +4,9 @@ import android.arch.lifecycle.ViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
+import hu.autsoft.rainbowcake.demo.ui.bar.BarViewModel
 import hu.autsoft.rainbowcake.demo.ui.example.ExampleViewModel
+import hu.autsoft.rainbowcake.demo.ui.foo.FooViewModel
 import hu.autsoft.rainbowcake.di.ViewModelKey
 
 @Suppress("unused")
@@ -14,6 +16,16 @@ abstract class ViewModelModule {
     @Binds
     @IntoMap
     @ViewModelKey(ExampleViewModel::class)
-    abstract fun bindExampleViewModel(listViewModel: ExampleViewModel): ViewModel
+    abstract fun bindExampleViewModel(exampleViewModel: ExampleViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(FooViewModel::class)
+    abstract fun bindFooViewModel(fooViewModel: FooViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(BarViewModel::class)
+    abstract fun bindBarViewModel(barViewModel: BarViewModel): ViewModel
 
 }
