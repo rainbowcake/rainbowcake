a36313a1d279d8b3a8f06b1f39e872ac3a5e1455
Braun Márton Szabolcs <braun.marton@autsoft.hu>
2019-01-19 19:48:45 +0100

    Add design library, update default Fragment layout

--- demo/src/main/java/hu/autsoft/rainbowcake/demo/ui/example/ExampleFragment.kt
+++ demo/src/main/java/hu/autsoft/rainbowcake/demo/ui/example/ExampleFragment.kt
@@ -1,13 +1,12 @@
 package hu.autsoft.rainbowcake.demo.ui.example
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.View
 import hu.autsoft.rainbowcake.base.BaseFragment
 import hu.autsoft.rainbowcake.base.getViewModelFromFactory
 import hu.autsoft.rainbowcake.demo.R
 import hu.autsoft.rainbowcake.demo.ui.foo.FooFragment
-import hu.autsoft.rainbowcake.extensions.exhaustive
+import hu.autsoft.rainbowcake.demo.ui.sharedvmpager.SharedVMPagerFragment
 import hu.autsoft.rainbowcake.navigation.navigator
 import kotlinx.android.synthetic.main.fragment_example.*
 
@@ -19,7 +18,11 @@ class ExampleFragment : BaseFragment<ExampleViewState, ExampleViewModel>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        exampleFragmentRoot.setOnClickListener {
+        sharedVMDemoButton.setOnClickListener {
+            navigator?.add(SharedVMPagerFragment())
+        }
+
+        customAnimationDemoButton.setOnClickListener {
             navigator?.add(FooFragment(),
                     enterAnim = R.anim.slide_in_right,
                     exitAnim = R.anim.slide_out_left,
@@ -28,21 +31,8 @@ class ExampleFragment : BaseFragment<ExampleViewState, ExampleViewModel>() {
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        viewModel.load()
-    }
-
     override fun render(viewState: ExampleViewState) {
-        when (viewState) {
-            is DataState -> {
-                // TODO render
-            }
-            NoDataState -> {
-                // TODO render
-            }
-        }.exhaustive
+
     }
 
 }
