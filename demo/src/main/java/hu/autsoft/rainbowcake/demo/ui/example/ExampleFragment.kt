d83282697a3f17ad2b34a0ff1e3423efb1695ab9
Braun Márton Szabolcs <braun.marton@autsoft.hu>
2019-01-19 19:32:59 +0100

    Remove navigation drawer

--- demo/src/main/java/hu/autsoft/rainbowcake/demo/ui/example/ExampleFragment.kt
+++ demo/src/main/java/hu/autsoft/rainbowcake/demo/ui/example/ExampleFragment.kt
@@ -3,13 +3,10 @@ package hu.autsoft.rainbowcake.demo.ui.example
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.View
-import co.zsmb.materialdrawerkt.builders.drawer
-import co.zsmb.materialdrawerkt.draweritems.badgeable.primaryItem
 import hu.autsoft.rainbowcake.base.BaseFragment
 import hu.autsoft.rainbowcake.base.getViewModelFromFactory
 import hu.autsoft.rainbowcake.demo.R
 import hu.autsoft.rainbowcake.demo.ui.foo.FooFragment
-import hu.autsoft.rainbowcake.demo.ui.sharedvmpager.SharedVMPagerFragment
 import hu.autsoft.rainbowcake.extensions.exhaustive
 import hu.autsoft.rainbowcake.navigation.navigator
 import kotlinx.android.synthetic.main.fragment_example.*
@@ -29,22 +26,6 @@ class ExampleFragment : BaseFragment<ExampleViewState, ExampleViewModel>() {
                     popEnterAnim = R.anim.slide_in_left,
                     popExitAnim = R.anim.slide_out_right)
         }
-
-        drawer {
-            toolbar = this@ExampleFragment.toolbar
-
-            selectedItemByPosition = -1
-
-            primaryItem("Shared VM with Pager") {
-                onClick(openFragment(::SharedVMPagerFragment))
-                selectable = false
-            }
-        }
-    }
-
-    private fun openFragment(fragmentCreator: () -> Fragment): (View?) -> Boolean = {
-        navigator?.add(fragmentCreator())
-        false
     }
 
     override fun onStart() {
