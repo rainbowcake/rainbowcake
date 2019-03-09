1e292287b3387330a22b2ec8d2b89f2f7e539f62
Braun Márton Szabolcs <braun.marton@autsoft.hu>
2019-03-09 19:28:15 +0100

    Remove Multidex initialization
    Signed-off-by: Braun Márton Szabolcs <braun.marton@autsoft.hu>

--- rainbow-cake/src/main/java/hu/autsoft/rainbowcake/BaseApplication.kt
+++ rainbow-cake/src/main/java/hu/autsoft/rainbowcake/BaseApplication.kt
@@ -1,9 +1,7 @@
 package hu.autsoft.rainbowcake
 
 import android.app.Application
-import android.content.Context
 import android.support.annotation.CallSuper
-import android.support.multidex.MultiDex
 import hu.autsoft.rainbowcake.base.InjectedActivity
 import hu.autsoft.rainbowcake.base.InjectedFragment
 import hu.autsoft.rainbowcake.di.BaseComponent
@@ -18,12 +16,6 @@ abstract class BaseApplication : Application() {
      * The injector used by [InjectedFragment] and [InjectedActivity]
      */
     abstract val injector: BaseComponent
-
-    @CallSuper
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-        MultiDex.install(this)
-    }
 
     @CallSuper
     override fun onCreate() {
