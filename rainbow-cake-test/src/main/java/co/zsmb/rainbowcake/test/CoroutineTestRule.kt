cd5b28e4ace7c582a96d2c825169caee97623c41
Márton Braun <braunmarci@gmail.com>
2019-11-01 10:38:05 +0100

    Add rainbow-cake-test module
    Signed-off-by: Márton Braun <braunmarci@gmail.com>

---
+++ rainbow-cake-test/src/main/java/co/zsmb/rainbowcake/test/CoroutineTestRule.kt
@@ -0,0 +1,44 @@
+package co.zsmb.rainbowcake.test
+
+import co.zsmb.rainbowcake.ioContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+/**
+ * A JUnit 4 test rule to be used in ViewModel and Presenter tests.
+ *
+ * Forces coroutine code that otherwise uses the Main or IO dispatchers
+ * to run on the single test thread.
+ */
+@UseExperimental(ExperimentalCoroutinesApi::class)
+class CoroutineTestRule : TestRule {
+
+    private val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+
+    override fun apply(base: Statement, description: Description?): Statement {
+        return object : Statement() {
+            override fun evaluate() {
+                // setup
+                @Suppress("DEPRECATION")
+                ioContext = testDispatcher
+                Dispatchers.setMain(testDispatcher)
+
+                // run test
+                base.evaluate()
+
+                // teardown
+                @Suppress("DEPRECATION")
+                ioContext = Dispatchers.IO
+                Dispatchers.resetMain()
+                testDispatcher.cleanupTestCoroutines()
+            }
+        }
+    }
+
+}
