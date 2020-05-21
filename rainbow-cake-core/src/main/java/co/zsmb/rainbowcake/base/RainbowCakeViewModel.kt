ad17c93af7e6f61070cf33686ba3cf2ea31da178
Márton Braun <braunmarci@gmail.com>
2020-05-21 18:44:49 +0200

    Deprecate JobViewModel
    Signed-off-by: Márton Braun <braunmarci@gmail.com>

--- rainbow-cake-core/src/main/java/co/zsmb/rainbowcake/base/RainbowCakeViewModel.kt
+++ rainbow-cake-core/src/main/java/co/zsmb/rainbowcake/base/RainbowCakeViewModel.kt
@@ -5,7 +5,9 @@ package co.zsmb.rainbowcake.base
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.support.annotation.CallSuper
 import android.support.annotation.VisibleForTesting
+import co.zsmb.rainbowcake.internal.config.RainbowCakeConfiguration
 import co.zsmb.rainbowcake.internal.livedata.ActiveOnlySingleShotLiveData
 import co.zsmb.rainbowcake.internal.livedata.ClairvoyantLiveData
 import co.zsmb.rainbowcake.internal.livedata.LiveDataCollection
@@ -14,13 +16,24 @@ import co.zsmb.rainbowcake.internal.livedata.MutableLiveDataCollectionImpl
 import co.zsmb.rainbowcake.internal.livedata.QueuedSingleShotLiveData
 import co.zsmb.rainbowcake.internal.livedata.SingleShotLiveData
 import co.zsmb.rainbowcake.internal.livedata.distinct
+import co.zsmb.rainbowcake.internal.logging.log
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 /**
- * A ViewModel base class that provides safe view state handling via [LiveData]
- * and one-time event support via [SingleShotLiveData].
+ * A ViewModel base class that provides:
+ * - safe view state handling via [LiveData],
+ * - one-time event support via [SingleShotLiveData],
+ * - the ability to easily start coroutines in a UI context.
  */
 abstract class RainbowCakeViewModel<VS : Any>(initialState: VS) : ViewModel() {
 
+    //region State
     /**
      * The [MutableLiveData] instance actually containing the current view state.
      */
@@ -67,8 +80,9 @@ abstract class RainbowCakeViewModel<VS : Any>(initialState: VS) : ViewModel() {
     protected fun postState(viewState: VS) {
         _state.postValue(viewState)
     }
+    // endregion
 
-
+    //region Events
     /**
      * The [ActiveOnlySingleShotLiveData] collection dispatching one-time events from
      * the ViewModel to the Fragment or Activity.
@@ -137,5 +151,112 @@ abstract class RainbowCakeViewModel<VS : Any>(initialState: VS) : ViewModel() {
     protected fun postQueuedEvent(event: QueuedOneShotEvent) {
         queuedViewEvents.setValue(event)
     }
+    //endregion
+
+    //region Coroutine support
+    /**
+     * An implementation of the [CoroutineScope] interface.
+     *
+     * Coroutines launched in this scope will get use the Main dispatcher (i.e.
+     * start on the UI thread) and have a SupervisorJob as their parent. This job
+     * is cancelled when the ViewModel is cleared, which also cancels all its
+     * children coroutines.
+     */
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
+
+    @CallSuper
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+        log("ViewModel cleared, job cancelled")
+    }
+
+    /**
+     * The flag indicating that a blocking [execute] is currently in progress.
+     * A primitive Boolean without synchronization is safe to use here since
+     * it will only ever be accessed on the UI thread.
+     */
+    private var busy = false
+
+    /**
+     * Executes the given [task] in a coroutine in the ViewModel's scope
+     * (on the UI thread, with cancellation if the ViewModel is cleared).
+     *
+     * These calls are "blocking" by default, in the sense that if a previous
+     * [execute] call is still ongoing (even if suspended or currently on
+     * another thread), any other [execute] calls will silently terminate
+     * as no-ops. This behaviour, for example, prevents launching duplicate
+     * actions on quick repeated button presses, which is a common issue.
+     *
+     * Any exceptions not caught by the [task] will be caught and consumed
+     * by this method.
+     *
+     * @param blocking Whether this [execute] call should block other job
+     *                 launches via [execute] until it completes.
+     */
+    @Suppress("RedundantUnitReturnType")
+    protected fun execute(blocking: Boolean = true, task: suspend () -> Unit): Unit {
+        executeImpl(blocking, task)
+    }
+
+    /**
+     * Convenience method for running [execute] in a non-blocking way with a
+     * (subjectively) cleaner syntax.
+     */
+    @Suppress("RedundantUnitReturnType")
+    protected fun executeNonBlocking(task: suspend () -> Unit): Unit {
+        executeImpl(blocking = false, task = task)
+    }
+
+    /**
+     * A variation of [execute] that returns the Job for the coroutine it
+     * started, mainly for manual coroutine cancellation purposes.
+     */
+    protected fun executeCancellable(blocking: Boolean = true, task: suspend () -> Unit): Job {
+        return executeImpl(blocking, task)
+    }
+
+    /**
+     * Actual private implementation of executing a given task. For details,
+     * see [execute], which is the usual entry point to this method.
+     */
+    private fun executeImpl(blocking: Boolean = true, task: suspend () -> Unit): Job {
+        return coroutineScope.launch {
+            if (blocking) {
+                if (busy) {
+                    log("Denying job launch, busy")
+                    return@launch
+                }
+                busy = true
+            }
+
+            try {
+                if (RainbowCakeConfiguration.consumeExecuteExceptions) {
+                    consumeExceptions {
+                        task()
+                    }
+                } else {
+                    task()
+                }
+            } finally {
+                if (blocking) {
+                    busy = false
+                }
+            }
+        }
+    }
+
+    private suspend fun consumeExceptions(task: suspend () -> Unit) {
+        try {
+            task()
+        } catch (e: CancellationException) {
+            log("Job cancelled exception:")
+            log(e)
+        } catch (e: Exception) {
+            log("Unhandled exception in ViewModel:")
+            log(e)
+        }
+    }
+    //endregion
 
 }
