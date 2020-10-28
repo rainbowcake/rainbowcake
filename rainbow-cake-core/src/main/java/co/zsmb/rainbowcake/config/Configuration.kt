d368598d1e47dc94184c7880d118879d288bde20
Márton Braun <braunmarci@gmail.com>
2020-10-28 18:32:51 +0100

    Remove the receiver of the rainbowCake configuration function
    Signed-off-by: Márton Braun <braunmarci@gmail.com>

--- rainbow-cake-core/src/main/java/co/zsmb/rainbowcake/config/Configuration.kt
+++ rainbow-cake-core/src/main/java/co/zsmb/rainbowcake/config/Configuration.kt
@@ -1,11 +1,10 @@
 package co.zsmb.rainbowcake.config
 
-import android.app.Application
 import co.zsmb.rainbowcake.internal.config.RainbowCakeConfiguration
 
 /**
  * The entry point of the Rainbow Cake configuration DSL.
  */
-public inline fun Application.rainbowCake(actions: RainbowCakeConfigurator.() -> Unit) {
+public inline fun rainbowCake(actions: RainbowCakeConfigurator.() -> Unit) {
     RainbowCakeConfiguration.actions()
 }
