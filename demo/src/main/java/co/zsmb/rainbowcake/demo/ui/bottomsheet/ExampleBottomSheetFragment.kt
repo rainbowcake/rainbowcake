616105369cb917e14de45bfcb244e47b9283d2ce
Márton Braun <braunmarci@gmail.com>
2020-11-30 21:46:01 +0100

    Fix superclass of ExampleBottomSheetFragment
    Signed-off-by: Márton Braun <braunmarci@gmail.com>

--- demo/src/main/java/co/zsmb/rainbowcake/demo/ui/bottomsheet/ExampleBottomSheetFragment.kt
+++ demo/src/main/java/co/zsmb/rainbowcake/demo/ui/bottomsheet/ExampleBottomSheetFragment.kt
@@ -1,13 +1,13 @@
 package co.zsmb.rainbowcake.demo.ui.bottomsheet
 
-import co.zsmb.rainbowcake.base.RainbowCakeDialogFragment
+import co.zsmb.rainbowcake.base.RainbowCakeBottomSheetFragment
 import co.zsmb.rainbowcake.dagger.getViewModelFromFactory
 import co.zsmb.rainbowcake.demo.R
 import co.zsmb.rainbowcake.demo.ui.example.ExampleViewModel
 import co.zsmb.rainbowcake.demo.ui.example.ExampleViewState
 import kotlinx.android.synthetic.main.fragment_example_bottom_sheet.*
 
-class ExampleBottomSheetFragment : RainbowCakeDialogFragment<ExampleViewState, ExampleViewModel>() {
+class ExampleBottomSheetFragment : RainbowCakeBottomSheetFragment<ExampleViewState, ExampleViewModel>() {
 
     override fun provideViewModel() = getViewModelFromFactory()
     override fun getViewResource() = R.layout.fragment_example_bottom_sheet
