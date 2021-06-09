6e56fa01f5dcd014fab68c902929db2fcebde9ef
Márton Braun <braunmarci@gmail.com>
2021-06-09 20:42:50 +0200

    Migrate Koin to 3.x version
    Signed-off-by: Márton Braun <braunmarci@gmail.com>

--- rainbow-cake-koin/src/main/java/co/zsmb/rainbowcake/koin/RainbowCakeActivity.kt
+++ rainbow-cake-koin/src/main/java/co/zsmb/rainbowcake/koin/RainbowCakeActivity.kt
@@ -2,7 +2,7 @@ package co.zsmb.rainbowcake.koin
 
 import co.zsmb.rainbowcake.base.RainbowCakeActivity
 import co.zsmb.rainbowcake.base.RainbowCakeViewModel
-import org.koin.android.viewmodel.ext.android.getViewModel
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 /**
  * Uses the global Koin instance to fetch the appropriate ViewModel instance for the Activity.
