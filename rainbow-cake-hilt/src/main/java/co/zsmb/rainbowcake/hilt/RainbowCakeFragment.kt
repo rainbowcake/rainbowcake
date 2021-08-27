2f18c41c093480b0a251320819ee4e43a6cf7acd
stewe93 <stewe93@gmail.com>
2021-08-27 09:59:35 +0200

    Add documentation to Hilt related extension methods

--- rainbow-cake-hilt/src/main/java/co/zsmb/rainbowcake/hilt/RainbowCakeFragment.kt
+++ rainbow-cake-hilt/src/main/java/co/zsmb/rainbowcake/hilt/RainbowCakeFragment.kt
@@ -3,18 +3,49 @@ package co.zsmb.rainbowcake.hilt
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import co.zsmb.rainbowcake.base.RainbowCakeBottomSheetDialogFragment
+import co.zsmb.rainbowcake.base.RainbowCakeDialogFragment
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.base.RainbowCakeViewModel
 import co.zsmb.rainbowcake.base.ViewModelScope
+import co.zsmb.rainbowcake.base.ViewModelScope.Activity
+import co.zsmb.rainbowcake.base.ViewModelScope.Default
+import co.zsmb.rainbowcake.base.ViewModelScope.ParentFragment
 
+/**
+ * Uses the HiltViewModelFactory provided by Hilt to
+ * fetch the appropriate ViewModel instance for the Fragment.
+
+ * @param scope The scope that the ViewModel should be fetched from and exist in.
+ *              See [ViewModelScope] for details.
+ */
 public inline fun <F : RainbowCakeFragment<VS, VM>, VS, reified VM : RainbowCakeViewModel<VS>> F.getViewModel(
-    scope: ViewModelScope = ViewModelScope.Default
+    scope: ViewModelScope = Default
 ): VM {
     return getFragmentViewModel(scope)
 }
 
+/**
+ * Uses the HiltViewModelFactory provided by Hilt to
+ * fetch the appropriate ViewModel instance for the Fragment.
+
+ * @param scope The scope that the ViewModel should be fetched from and exist in.
+ *              See [ViewModelScope] for details.
+ */
+public inline fun <F : RainbowCakeDialogFragment<VS, VM>, VS, reified VM : RainbowCakeViewModel<VS>> F.getViewModelFromFactory(
+    scope: ViewModelScope = Default
+): VM {
+    return getFragmentViewModel(scope)
+}
+
+/**
+ * Uses the HiltViewModelFactory provided by Hilt to
+ * fetch the appropriate ViewModel instance for the Fragment.
+
+ * @param scope The scope that the ViewModel should be fetched from and exist in.
+ *              See [ViewModelScope] for details.
+ */
 public inline fun <F : RainbowCakeBottomSheetDialogFragment<VS, VM>, VS, reified VM : RainbowCakeViewModel<VS>> F.getViewModel(
-    scope: ViewModelScope = ViewModelScope.Default
+    scope: ViewModelScope = Default
 ): VM {
     return getFragmentViewModel(scope)
 }
@@ -22,10 +53,10 @@ public inline fun <F : RainbowCakeBottomSheetDialogFragment<VS, VM>, VS, reified
 @PublishedApi
 internal inline fun <reified VM : RainbowCakeViewModel<VS>, VS> Fragment.getFragmentViewModel(scope: ViewModelScope): VM {
     return when (scope) {
-        is ViewModelScope.Default -> {
+        is Default -> {
             ViewModelProvider(this).get(VM::class.java)
         }
-        is ViewModelScope.ParentFragment -> {
+        is ParentFragment -> {
             val parentFragment = parentFragment ?: throw IllegalStateException("No parent Fragment")
             val key = scope.key
             if (key != null) {
@@ -34,7 +65,7 @@ internal inline fun <reified VM : RainbowCakeViewModel<VS>, VS> Fragment.getFrag
                 ViewModelProvider(parentFragment).get(VM::class.java)
             }
         }
-        is ViewModelScope.Activity -> {
+        is Activity -> {
             val key = scope.key
             if (key != null) {
                 ViewModelProvider(requireActivity()).get(key, VM::class.java)
