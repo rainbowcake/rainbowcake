5de5683da2a1e7ec9c6dedf5bd85561e373090c1
Márton Braun <braunmarci@gmail.com>
2021-08-30 22:13:34 +0200

    Fix factory method names in Hilt module
    Signed-off-by: Márton Braun <braunmarci@gmail.com>

--- hilt-demo/src/main/java/co/zsmb/rainbowcake/hiltdemo/blank/BlankFragment.kt
+++ hilt-demo/src/main/java/co/zsmb/rainbowcake/hiltdemo/blank/BlankFragment.kt
@@ -3,14 +3,14 @@ package co.zsmb.rainbowcake.hiltdemo.blank
 import android.os.Bundle
 import android.view.View
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
-import co.zsmb.rainbowcake.hilt.getViewModel
+import co.zsmb.rainbowcake.hilt.getViewModelFromFactory
 import co.zsmb.rainbowcake.hiltdemo.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class BlankFragment : RainbowCakeFragment<BlankViewState, BlankViewModel>() {
 
-    override fun provideViewModel() = getViewModel()
+    override fun provideViewModel() = getViewModelFromFactory()
     override fun getViewResource() = R.layout.fragment_blank
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
